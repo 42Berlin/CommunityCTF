3c0ac937b0f71b86606589de00f5d406c5739d9e
Nathan Guiard <nathan@42berlin.de>
2023-09-22 10:45:36 +0200

    Dockerize "what?"

--- what?/bash_questionmark.py
+++ what?/bash_questionmark.py
@@ -7,15 +7,18 @@ not_allowed = '()$[];/|<>{}\'"!\\~`&'
 
 @server.get('/{arg}')
 def get_arg(req: Request, arg: str):
-    if 'flag.txt' in arg.lower():
-        return Response('You cannot have "flag.txt" in your path', 403)
+    if 'deepest_secret.txt' in arg.lower():
+        return Response('You cannot have "deepest_secret.txt" in your path', 403)
 
     for i in not_allowed:
         if i in arg:
             return Response('Not authorized characters in the path. List of non-authorised caracters: ' + not_allowed, 403)
     ret = subprocess.run(f'cat {arg}', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return Response(str(ret.stdout) + str(ret.stderr), 200)
+    return Response(ret.stdout.decode("utf-8") + ret.stderr.decode("utf-8"), 200)
 
 @server.get('/')
 def get_root():
-    return Response('GET / needs an argument, example /hello')
+    ret = subprocess.run(f'ls *.txt', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return Response("GET / needs an argument, example /hello.\n"
+                    "Files available in the directory\n\n" +
+                    ret.stdout.decode("utf-8"))
