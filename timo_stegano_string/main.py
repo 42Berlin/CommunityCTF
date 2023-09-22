713843110adc1e44bd03a6c0853a1d0559ade018
Nathan Guiard <nathan@42berlin.de>
2023-09-22 16:46:58 +0200

    Added content for timo's challenge

--- timo_stegano_string/main.py
+++ timo_stegano_string/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Response
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/', response_class = HTMLResponse)
 def get_root():
     f = open('content.html', 'r')
     content = f.read()
 
-    return Response(content, 200)
+    return HTMLResponse(content, 200)
